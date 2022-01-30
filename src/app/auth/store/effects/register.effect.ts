60f84469b3830fdfdced4b8bed6f345dcc8976d9
marmelatta <testandroidapp@yandex.ru>
2022-01-30 20:54:40 +0500

    redirect in effect

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,16 +1,18 @@
 import {Injectable} from "@angular/core";
 import {Actions, createEffect, ofType} from "@ngrx/effects";
 import {registerAction, registerFailureAction, registerSuccessAction} from "../actions/register.action";
-import {catchError, map, of, switchMap} from "rxjs";
+import {catchError, map, of, switchMap, tap} from "rxjs";
 import {AuthService} from "../../services/auth.service";
 import {CurrentUserInterface} from "../../../shared/types/currentUser.interface";
 import {HttpErrorResponse} from "@angular/common/http";
 import {PersistanceService} from "../../../shared/services/persistance.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class RegisterEffect {
   constructor(private actions$: Actions, private authService: AuthService,
-              private persistanceService: PersistanceService) {}
+              private persistanceService: PersistanceService,
+              private router: Router) {}
 
   register$ = createEffect(() => this.actions$.pipe(
     ofType(registerAction), //фильтруем сводя до нескольких экшнов или до одного
@@ -24,4 +26,13 @@ export class RegisterEffect {
       })
     ))
   ))
+
+  redirectAfterSubmit$ = createEffect(() => this.actions$.pipe(
+    ofType(registerSuccessAction),
+    tap(() => {
+      this.router.navigateByUrl('/');
+    })
+  ),
+    {dispatch: false} // не возвращает action
+  )
 }
